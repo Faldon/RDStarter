21386c28f33d085e41740d21f0cf63bd185a8dee
Faldon <faldon@thesecretgamer.de>
2014-03-13 16:05:06 +0100

    Initial Commit. Working Project.

---
+++ RDStarter/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+
+namespace RDStarter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            List<string> devices = getTargetDevices();
+            InitializeComponent();
+            this.TargetChoice.ItemsSource = devices;
+        }
+
+        private void connectClick(Object sender, EventArgs e)
+        {
+            bool requestControl = (bool)this.control.IsChecked;
+            bool requestConsent = (bool)this.consent.IsChecked;
+            string DeviceName = this.TargetChoice.SelectedItem.ToString();
+
+            connectToDevice(DeviceName, requestControl, requestConsent);
+        }
+
+        /*
+         * Returns a List containing the dns name of all computers in the current
+         * active directory running Windows 8.1 (Version 6.3 Build 9600)
+         */
+        private static List<string> getTargetDevices()
+        {
+            List<string> targetDevices = new List<string>();
+            DirectorySearcher ds = new DirectorySearcher();
+            ds.Filter = ("(objectClass=computer)");
+
+            foreach(SearchResult item in ds.FindAll())
+            {
+                String osVersion = item.GetDirectoryEntry().Properties["operatingSystemVersion"].Value.ToString();
+                if (osVersion.Equals("6.3 (9600)"))
+                {
+                    targetDevices.Add(item.GetDirectoryEntry().Properties["dNSHostName"].Value.ToString());
+                };
+            }
+            return targetDevices;
+        }
+
+        /*
+         * Connects to the selected computer through mstsc and shadowing
+         */
+        private static void connectToDevice(string DeviceName, bool requestControl, bool requestConsent)
+        {
+            string cmdString = "/C mstsc /shadow:1 /v:" + DeviceName;
+            if (requestControl) { cmdString += " /control";  }
+            if (!requestConsent) { cmdString += " /noConsentPrompt"; }
+
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo si = new System.Diagnostics.ProcessStartInfo();
+            si.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            si.FileName = "cmd.exe";
+            si.Verb = "runas";
+            si.Arguments = cmdString;
+            
+            p.StartInfo = si;
+            p.Start();
+        }
+    }
+}
